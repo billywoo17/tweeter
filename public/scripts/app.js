933aa2bd2ad1e5634938101ff2a28327fa8b6e88
bill <billwu_@outlook.com>
2018-04-25 17:56:19 +0000

    added moment.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,45 +1,84 @@
 
 function createTweetElement(data){
+//header
+  let img = $("<img/>").attr("src", data["user"]["avatars"]["regular"]);
+  let h1 = $("<h1/>").text(data["user"]["name"]);
+  let h2 = $("<h2/>").text(data["user"]["handle"]);
 
-
+  let header = $("<header/>").append(img, h1, h2);
 
 //footer
   let h4 = $("<h4>").text(data["created_at"]);
   let retweet = $("<i class='fas fa-retweet' id='retweet'></i>");
-  let heart = $("<i class='fa fa-heart'></i>")
-  let flag = $("<i class='fab fa-font-awesome-flag' id='flag'></i>");
+  let heart = $("<i class='fas fa-heart'></i>");
+  let flag = $("<i class='fas fa-flag' id='flag'></i>");
 
-  let footer = $("footer").append(h4);
-  footer = $("footer").append(retweet);
-  footer = $("footer").append(heart);
-  footer = $("footer").append(flag);
+  let footer = $("<footer/>").append(h4, retweet, heart, flag);
 
-  $teet = footer;
-  return $teet;
+//article
+  let h3 = $("<h3/>").text(data["content"]["text"]);
+
+  let article = $("<article/>").append(header, h3, footer);
+
+  return article;
 }
+function renderTweets(tweets) {
+  tweets.forEach(function(value) {
+  let $tweet = createTweetElement(value);
+  $('#tweets-container').append($tweet);
+  });
 
+}
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
 
 $(function() {
-  var $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweetets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
 });
 
