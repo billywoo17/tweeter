e184edd78d86c8db245e7ec21df8eb3eeb6162f8
bill <billwu_@outlook.com>
2018-04-25 03:08:23 +0000

    finished appending footer

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,45 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
+function createTweetElement(data){
+
+
+
+//footer
+  let h4 = $("<h4>").text(data["created_at"]);
+  let retweet = $("<i class='fas fa-retweet' id='retweet'></i>");
+  let heart = $("<i class='fa fa-heart'></i>")
+  let flag = $("<i class='fab fa-font-awesome-flag' id='flag'></i>");
+
+  let footer = $("footer").append(h4);
+  footer = $("footer").append(retweet);
+  footer = $("footer").append(heart);
+  footer = $("footer").append(flag);
+
+  $teet = footer;
+  return $teet;
+}
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+$(function() {
+  var $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweetets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
 
