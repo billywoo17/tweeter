a53acd8c658d6e52f801c3a226189aafe0b307c4
bill <billwu_@outlook.com>
2018-04-25 00:03:49 +0000

    working counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-// $("#inputTweet").on('keydown', function() {
-//   console.log(this.value.length);
-// });
+  $("#inputTweet").on('input', function() {
+    let letterCount = $(".counter");
+    let inputCount = $(this).val().length;
 
-$("#inputTweet").on('keyup', function() {
-  console.log(this);
-  let letterCount = document.querySelector("#counter")[0];
-  letterCount = (140- this.value.length);
-  if(letterCount < 0){
-  document.getElementById("#counter")[0].style.backgroundColor = "red";
-
-  }
-});
-
-
+    letterCount.text(140 - inputCount);
+    if(letterCount.text() < 0){
+      letterCount.css({color: "red"});
+    }
+  });
 });
